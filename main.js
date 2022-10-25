b24e1530dbf0eb647b3942c58683a7420b058efe
psycholemon <116676845+psycholemon@users.noreply.github.com>
2022-10-25 17:52:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+var entered = prompt("Password")
+var password = "lemons4life"
 
+if (entered == password) {
+  alert(Welcome back);
+} else {
+  alert(intruder...);
+  window.location.replace("https://psycholemon.github.io/");
