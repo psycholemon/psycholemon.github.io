5420557400d9a5f512b98701cada741b9dedb6be
huycool2011 <131486914+huycool2011@users.noreply.github.com>
2023-04-26 08:03:12 -0500

    Update Password

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var askForPW = prompt("Passwords")
-var password = "1234"
+var password = "Derekisnothim"
 if (password == askForPW) {
   alert("Correct")
-} else if (askForPW = 'LEMONS4LIFE'){
+} else if (askForPW = "LEMONS4LIFE"){
   alert("Welcome, Admin")
 } else {
   window.location.replace("http://www.psycholemon.github.io");
