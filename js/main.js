b0dd958a29b4bd4f5e29267a9712e6f2daf9dbd3
Cakerman12 <131909322+Cakerman12@users.noreply.github.com>
2023-04-27 14:43:00 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ window.onload=function(){
  if (password == "LEMONS4LIFE"){
   document.getElementById("ID").innerHTML = password;
  }else{
-  location.href = 'https://www.google.com';
+  location.href = 'https://www.youtube.com/shorts/96GnOB1iZQI';
  }
 }
