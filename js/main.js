853dd88dd734cae7e8e1cb23447bfa50da682871
huycool2011 <131486914+huycool2011@users.noreply.github.com>
2023-04-26 21:26:33 -0500

    password = LEMONS4LIFE

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
- window.onload=function(){
-var password = window.prompt("Enter your password","");
-     if (password == "LEMONS4LIFE"){
-                  document.getElementById("ID").innerHTML = password;
-     }else{
-     location.href = 'google.com';
-     }
+window.onload=function(){
+ var password = window.prompt("Enter your password","");
+ if (password == "LEMONS4LIFE"){
+  document.getElementById("ID").innerHTML = password;
+ }else{
+  location.href = 'google.com';
+ }
+}
